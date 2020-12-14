859e4e5d1f19daa95d1db4164aaeda1f48d6c774
Ceci2003 <progplay2003@gmail.com>
2020-12-14 17:08:03 +0200

    Added home components

--- fictional-land/src/app/home/home.module.ts
+++ fictional-land/src/app/home/home.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ControlMenuComponent } from './control-menu/control-menu.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PrivacyPageComponent } from './privacy-page/privacy-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AboutPageComponent, ContactPageComponent, ControlMenuComponent, HomePageComponent, PrivacyPageComponent],
   imports: [
     CommonModule
   ]
